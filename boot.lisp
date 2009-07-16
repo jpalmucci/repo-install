560cc2825615759085ac7524178acdeb19da0029
Jeff Palmucci <jpalmucci@csail.mit.edu>
2009-07-16 09:35:00 -0400

    Fix a startup issue with sbcl

--- boot.lisp
+++ boot.lisp
@@ -1,4 +1,10 @@
 
+;; load the old version of asdf so we won't get an undefined function error
+;; when sbcl tries to use ASDF::MODULE-PROVIDE-ASDF, which doesn't seem to be
+;; in the newest release of asdf
+#+sbcl
+(require :asdf)
+
 (load (merge-pathnames (make-pathname :directory '(:relative ".." "asdf") :name "asdf")
 		       *load-truename*))
 
