6425b3a48a02f350611b285db8e4d316cfa7fab1
Jeff Palmucci <jpalmucci@odd.local>
2009-09-24 12:31:17 -0400

    Switch off of using the central-registry because it doesn't work well under windows

--- boot.lisp
+++ boot.lisp
@@ -8,9 +8,29 @@
 (load (merge-pathnames (make-pathname :directory '(:relative ".." "asdf") :name "asdf")
 		       *load-truename*))
 
-(pushnew (make-pathname :directory (append (butlast (pathname-directory *load-truename*)) '("systems")))
-	 asdf:*central-registry*)
-
-(asdf:load-system :repo-install)
+;; windows doen't support symbolic links, so we don't use them in repo-install
+;; Set up the central registry temporarily so we can bootstrap repo-install
+(let ((asdf:*central-registry* 
+       (append asdf:*central-registry*
+		(mapcar #'(lambda (subdir)
+			    (make-pathname :directory (append (butlast (pathname-directory *load-truename*))
+							      (list subdir))
+					   :device (pathname-device *load-truename*)))
+			'("repo-install" "cl-fad" "trivial-shell" "cl-ppcre")))))
+  (asdf:load-system :repo-install))
 
 (load (merge-pathnames (make-pathname :name "most-recent-manifest" :type "lisp") *load-truename*))
+
+(in-package :ri) 
+
+;; tell asdf how to load repo-install packages
+(defun asd-file-for-system (system)
+  (let ((repo (find-repo system)))
+    (if (null repo)
+        (return-from asd-file-for-system nil))
+    (cond ((not (probe-file (asd-file repo system)))
+           (format t "Downloading ~a...~%" system)
+           (update-repo repo)))
+    (asd-file repo system)))
+
+(pushnew 'asd-file-for-system asdf:*system-definition-search-functions*)
